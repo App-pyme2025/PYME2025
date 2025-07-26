28f8e0a416b3d4e1ac520fbad632835af0ab84c1
App-pyme2025 <moni.thor.mex@gmail.com>
2025-07-25 19:31:14 -0600

    Sincronizando cambios locales

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,6 +1,7 @@
+// src/supabaseClient.js
 import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm';
 
-const SUPABASE_URL = 'https://hcothkyelfhoveehjfhc.supabase.co';
-const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imhjb3Roa3llbGZob3ZlZWhqZmhjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTM0NzY0NjIsImV4cCI6MjA2OTA1MjQ2Mn0.C4Kowt9AvLCah04QN0CF0Dhb7TuPinZIGkHlKTgyDnw';
+const SUPABASE_URL  = 'https://hcothkyelfhoveehjfhc.supabase.co';
+const SUPABASE_ANON = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imhjb3Roa3llbGZob3ZlZWhqZmhjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTM0NzY0NjIsImV4cCI6MjA2OTA1MjQ2Mn0.C4Kowt9AvLCah04QN0CF0Dhb7TuPinZIGkHlKTgyDnw';
 
-export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON);
