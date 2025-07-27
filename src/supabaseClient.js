6b8d56cdab6c300aab1274c640c709ef7fefb552
App-pyme2025 <moni.thor.mex@gmail.com>
2025-07-26 19:07:44 -0600

    Sincronizando cambios locales

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -2,6 +2,6 @@
 import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm';
 
 const SUPABASE_URL  = 'https://nyzlexsevgdwxgsarsfm.supabase.co';
-const SUPABASE_ANON = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imhjb3Roa3llbGZob3ZlZWhqZmhjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTM0NzY0NjIsImV4cCI6MjA2OTA1MjQ2Mn0.C4Kowt9AvLCah04QN0CF0Dhb7TuPinZIGkHlKTgyDnw';
+const SUPABASE_ANON = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im55emxleHNldmdkd3hnc2Fyc2ZtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTM0OTA2NTEsImV4cCI6MjA2OTA2NjY1MX0.ZOKdhOvtaalHNOYTUDAGy4aO65tJ50L0VQcJk1Vl_Co';
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON);
